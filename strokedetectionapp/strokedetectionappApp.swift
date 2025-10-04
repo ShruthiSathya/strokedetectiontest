b2f0fdf57706dcc15887d23530eb1b8a7b6e199c
shruthi <sruthi2k6@gmail.com>
2025-10-04 16:05:32 -0700

    Initial Commit

---
+++ strokedetectionapp/strokedetectionappApp.swift
@@ -0,0 +1,17 @@
+//
+//  strokedetectionappApp.swift
+//  strokedetectionapp
+//
+//  Created by Shruthi Sathya on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct strokedetectionappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ strokedetectionappTests/strokedetectionappTests.swift
@@ -0,0 +1,17 @@
+//
+//  strokedetectionappTests.swift
+//  strokedetectionappTests
+//
+//  Created by Shruthi Sathya on 10/4/25.
+//
+
+import Testing
+@testable import strokedetectionapp
+
+struct strokedetectionappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
